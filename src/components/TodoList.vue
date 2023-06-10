8bff385acdae4b5af73ae924fad2f2d34cbcfa96
damin <kimura.r.ah@m.titech.ac.jp>
2023-06-10 18:35:49 +0900

    todoリスト作成

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,56 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+interface Task {
+  name: string
+  status: string
+}
+
+const tasks = ref<Task[]>([
+  { name: '英語の課題', status: '未完了' },
+  { name: '有機化学の課題', status: '完了済み' }
+])
+const newTaskName = ref('')
+const newTaskStatus = ref('')
+
+const addTask = () => {
+  tasks.value.push({ name: newTaskName.value, status: newTaskStatus.value })
+}
+</script>
 
 <template>
-  <div>TodoList</div>
+  <div>
+    <h2>TodoList</h2>
+    <div>未完了</div>
+    <ul v-for="task in tasks" :key="task.name">
+      <li v-if="task.status == '未完了'">
+        <div>
+          {{ task.name }}
+          <!-- <div>状態: {{ task.status }}</div> -->
+          <button @click="task.status='完了済み'">✔️</button>
+        </div>
+      </li>
+    </ul>
+    <div>完了済み</div>
+    <ul v-for="task in tasks" :key="task.name">
+      <li v-if="task.status == '完了済み'">
+        <div>{{ task.name }}</div>
+        <!-- <div>状態: {{ task.status }}</div> -->
+      </li>
+    </ul>
+    <div>
+      <label>
+        タスク
+        <input v-model="newTaskName" type="text" />
+      </label>
+      <label>
+        状態
+        <input v-model="newTaskStatus" type="text" />
+      </label>
+      <button v-if="newTaskName!=''" @click="addTask(); newTaskName=''; newTaskStatus=''">追加</button>
+      <button v-else>追加</button>
+    </div>
+  </div>
 </template>
 
 <style></style>
