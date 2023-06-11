a3fd5155c38b9200fcf0589a0e0aa77600ffa3d0
damin <kimura.r.ah@m.titech.ac.jp>
2023-06-11 13:05:09 +0900

    期限の入力設定と重複タスクの拒否

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,46 +4,77 @@ import { ref } from 'vue'
 interface Task {
   name: string
   status: string
+  limit: string
 }
 
 const tasks = ref<Task[]>([
-  { name: '英語の課題', status: '未完了' },
-  { name: '有機化学の課題', status: '完了済み' }
+  { name: '英語の課題', status: '未完了', limit: '6/12' },
+  { name: '有機化学の課題', status: '完了済み', limit: '' }
 ])
+
 const newTaskName = ref('')
 const newTaskStatus = ref('未完了')
+const newTaskLimit = ref('')
 
 const addTask = () => {
-  tasks.value.push({ name: newTaskName.value, status: newTaskStatus.value })
+  tasks.value.push({ name: newTaskName.value, status: newTaskStatus.value, limit: newTaskLimit.value})
+}
+
+const duplicateCheck = (name_:string) => {
+  let flag = false
+  for (let i = 0; i < tasks.value.length; ++i){
+    if (name_==tasks.value[i].name){
+      flag = true
+    }
+  }
+  return flag
+}
+
+const deleteTask = (x:number) => {
+  tasks.value.splice(x,1)
 }
 </script>
 
 <template>
   <div>
     <h2>TodoList</h2>
-    <div>未完了</div>
-    <ul v-for="task in tasks" :key="task.name">
-      <li v-if="task.status == '未完了'">
-        <div>
-          {{ task.name }}
-          <!-- <div>状態: {{ task.status }}</div> -->
-          <button @click="task.status='完了済み'">✔️</button>
-        </div>
-      </li>
-    </ul>
-    <div>完了済み</div>
-    <ul v-for="task in tasks" :key="task.name">
-      <li v-if="task.status == '完了済み'">
-        <div>{{ task.name }}</div>
-        <!-- <div>状態: {{ task.status }}</div> -->
-      </li>
-    </ul>
+    <h3>未完了</h3>
+      <div v-if="tasks.filter(t => t.status == '未完了').length==0">未完了のタスクはありません</div>
+      <ul v-for="(task, index) in tasks" :key="task.name">
+        <li v-if="task.status=='未完了'">
+          <div v-if="task.limit == ''">
+            {{ task.name }}（期限：なるべく早く）
+            <button @click="task.status='完了済み' ">✔️</button>
+            <button @click="deleteTask(index)">削除</button>
+          </div>
+          <div v-else>
+            {{ task.name }}（期限：{{ task.limit }}）
+            <button @click="task.status='完了済み' ">✔️</button>
+            <button @click="deleteTask(index)">削除</button>
+          </div>
+        </li>
+      </ul>
+    <h3>完了済み</h3>
+      <ul v-for="(task, index) in tasks" :key="task.name">
+        <li v-if="task.status=='完了済み'">
+          <div>
+            {{ task.name }}
+            <button @click="deleteTask(index)">削除</button>
+          </div>
+        </li>
+      </ul>
+      <label>
+        タスク内容
+        <input v-model="newTaskName" type="text" placeholder="タスクをここに入力" />
+        <div v-if="duplicateCheck(newTaskName)">⚠️このタスクはすでに追加されています</div>
+      </label>
     <div>
       <label>
-        タスク
-        <input v-model="newTaskName" type="text" />
+        期限
+        <input v-model="newTaskLimit" type="text" placeholder="期限をここに入力" />
       </label>
-      <button v-if="newTaskName!=''" @click="addTask(); newTaskName=''">追加</button>
+      <button v-if="duplicateCheck(newTaskName)">追加</button>
+      <button v-else-if="newTaskName!=''" @click="addTask(); newTaskName=''">追加</button>
       <button v-else>追加</button>
     </div>
   </div>
